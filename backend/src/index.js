b93c854741295bd0dd32197c22689b9724b8083b
Moises56 <moiavil56@gmail.com>
2021-04-06 16:17:32 -0600

    backen+Frontend=connect2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const cors = require('cors');
 
@@ -10,8 +11,13 @@ app.use(express.json()); //?para leer los datos quue se estan resiviendo
 app.use('/api', require('./routes/index'))
 
 
+app.use(express.static(__dirname+'/SoMoRa-Lance/dist/somoralancer'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/SoMoRa-Lance/dist/somoralancer/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
 
 
-
-app.listen(3000);
-console.log('Servidor en puerto 3000');
+// app.listen(3000);
+console.log('Servidor en puerto 8080');

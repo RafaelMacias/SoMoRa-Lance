0af45d55f184807ca392a306382d3a7073b9c496
unknown <moiavil56@gmail.com>
2021-04-06 17:21:30 -0600

    heroku2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,12 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
+// if (process.env.NODE_ENV !== 'production'){
+//     require('dotenv').config();
+// }
 
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
 require('./database');
 
 app.use(cors());
@@ -10,8 +15,9 @@ app.use(express.json()); //?para leer los datos quue se estan resiviendo
 app.use('/api', require('./routes/index'))
 
 
+app.use(express.static(path.join(__dirname, '/SoMoRa-Lance/dist/somoralancer')))
 
-
-
-app.listen(3000);
-console.log('Servidor en puerto 3000');
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), () => {
+    console.log('Servidor en puerto', app.get('port'));
+});

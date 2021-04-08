93b8664e5f98a42a907db1de9a4ae3d3cc5480a1
Moises56 <moiavil56@gmail.com>
2021-04-07 19:42:23 -0600

    hero3

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,13 +11,7 @@ app.use(express.json()); //?para leer los datos quue se estan resiviendo
 app.use('/api', require('./routes/index'))
 
 
-app.use(express.static(__dirname+'/SoMoRa-Lance/dist/somoralancer'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/SoMoRa-Lance/dist/somoralancer/index.html'));
-});
-
-app.listen(process.env.PORT || 8080);
 
 
-// app.listen(3000);
-console.log('Servidor en puerto 8080');
+app.listen(3000);
+console.log('Servidor en puerto 3000');

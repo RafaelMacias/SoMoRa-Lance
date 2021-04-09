7db8e43ddd0832b182b6cc67d0527355cf971850
unknown <moiavil56@gmail.com>
2021-04-08 22:31:12 -0600

    heroku5

--- SoMoRa-Lance/server.js
+++ SoMoRa-Lance/server.js
@@ -4,16 +4,17 @@ const path = require('path');
 
 const app = express();
 
-// require('../backend/src/index')
+require('../backend/src/index')
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/somoralancer'));
 
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/somoralancer/'}),
+    res.sendFile('index.html', {root: './dist/somoralancer'}),
 );
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
+module.exports = app;
